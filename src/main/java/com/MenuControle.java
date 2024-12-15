f9d39a635c44f9ef802a28a7cc2020a566129c7c
Vitor <vitordevestudos@gmail.com>
2024-12-15 17:18:37 -0300

    first commit

---
+++ src/main/java/com/MenuControle.java
@@ -0,0 +1,43 @@
+package com;
+
+import java.io.IOException;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author Jose
+ */
+public class MenuControle {
+    
+    @FXML
+    private void producaoIncluir() throws IOException{
+        App.setRoot("producaoIncluir");
+    }
+    
+    @FXML
+    private void producaoListarPorVaca() throws IOException{
+        App.setRoot("producaoListarPorVaca");
+    }
+    
+    @FXML
+    private void sair(){
+        Platform.exit();
+        System.exit(0);
+    }
+    
+    @FXML
+    private void vacaAlterar() throws IOException{
+        App.setRoot("vacaAlterar");
+    }
+    
+    @FXML
+    private void vacaIncluir() throws IOException{
+        App.setRoot("vacaIncluir");
+    }
+     
+    @FXML
+    private void usuarioIncluir() throws IOException {
+        App.setRoot("usuarioIncluir");
+    }
+}
